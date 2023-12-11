a285f6baad476fdd45be3fb4457680f3eb4975b6
Felicia <feelicianorman@hotmail.com>
2023-12-11 15:34:43 +0100

    installed ngx-markdown

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule, SecurityContext } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MarkdownModule } from 'ngx-markdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 import { GraphQLModule } from './graphql.module';
-import { HttpClientModule } from '@angular/common/http';
-import { LandingpageComponent } from './pages/landingpage/landingpage.component';
-import { SupportUsComponent } from './pages/support-us/support-us.component';
-import { KnowledgeComponent } from './pages/knowledge/knowledge.component';
+import { NavigationComponent } from './navigation/navigation.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ForpatientsComponent } from './pages/forpatients/forpatients.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { FooterComponent } from './footer/footer.component';
-import { NewsComponent } from './sub-pages/news/news.component';
+import { KnowledgeComponent } from './pages/knowledge/knowledge.component';
+import { LandingpageComponent } from './pages/landingpage/landingpage.component';
+import { SupportUsComponent } from './pages/support-us/support-us.component';
 import { BlogComponent } from './sub-pages/blog/blog.component';
 import { DonateComponent } from './sub-pages/donate/donate.component';
+import { NewsComponent } from './sub-pages/news/news.component';
 import { RegisterComponent } from './sub-pages/register/register.component';
 import { ShopComponent } from './sub-pages/shop/shop.component';
 
@@ -34,7 +34,16 @@ import { ShopComponent } from './sub-pages/shop/shop.component';
     RegisterComponent,
     ShopComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, GraphQLModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({
+      sanitize: SecurityContext.NONE,
+    }),
+    MarkdownModule.forChild(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
