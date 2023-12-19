bfedc6683820e8cbb3255a433cbc6435330ffbc6
Felicia <feelicianorman@hotmail.com>
2023-12-19 12:53:11 +0100

    used service

--- frontend/src/app/sub-pages/register/register.component.ts
+++ frontend/src/app/sub-pages/register/register.component.ts
@@ -1,6 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Apollo, gql } from 'apollo-angular';
 import { User } from 'src/app/models/User';
+import { RegisterService } from 'src/app/services/registerService';
 
 @Component({
   selector: 'app-register',
@@ -9,10 +12,21 @@ import { User } from 'src/app/models/User';
 })
 export class RegisterComponent implements OnInit {
   public newUser: User[] = [];
+  public user: User;
   form: FormGroup;
 
+  readonly ROOT_URL = 'http://localhost:1337/api/registers';
+
+  constructor(
+    private http: HttpClient,
+    private apollo: Apollo,
+    private apolloService: RegisterService,
+  ) {}
+
   addUser() {
-    console.log(this.form.value);
+    this.apolloService.createRegister(this.form.value).subscribe((result) => {
+      console.log(result);
+    });
   }
 
   ngOnInit(): void {
