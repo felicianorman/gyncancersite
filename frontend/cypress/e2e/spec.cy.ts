425ff50fddc647610d6f6e369fa036a2ca173d94
Felicia <feelicianorman@hotmail.com>
2023-12-20 19:10:28 +0100

    did my first strapi api test

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
-  })
-})
+    cy.visit('/');
+    cy.contains('Ingen ska dö i cancer - kunskap räddar liv');
+  });
+});
