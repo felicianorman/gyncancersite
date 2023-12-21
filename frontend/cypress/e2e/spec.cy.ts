093915925381a087cff6dd03c3266d44ce7cdc00
Felicia <feelicianorman@hotmail.com>
2023-12-21 11:52:45 +0100

    fixed bug with cypress

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,6 +1,33 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/');
-    cy.contains('Ingen ska dö i cancer - kunskap räddar liv');
+    // Make an API request
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:1337/graphql',
+      body: {
+        query: `query getShareSite {
+          lpSharesite {
+            data {
+              attributes {
+                title
+                content
+              }
+            }
+          }
+        }`,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => {
+      // Handle the API response and perform actions
+      const { title, content } = response.body.data.lpSharesite.data.attributes;
+      cy.wait(1000);
+      // Visit the site after the API request is complete
+      cy.visit('/');
+
+      // Perform actions on the visited page based on API response
+      cy.contains(content);
+    });
   });
 });
