3c6c1e09aa3a978d65a9f3225d389bba3ed2c87b
Felicia <feelicianorman@hotmail.com>
2024-01-07 20:06:37 +0100

    added styling basics

---
+++ my-project/src/api/kunskap/controllers/kunskap.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * kunskap controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::kunskap.kunskap');
---
+++ my-project/src/api/kunskap/routes/kunskap.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * kunskap router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::kunskap.kunskap');
