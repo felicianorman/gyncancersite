6750ba5a83d4bde3d0e1a087f061bd0221d2e015
Felicia <feelicianorman@hotmail.com>
2024-01-13 16:56:35 +0100

    cors

--- my-project/config/middlewares.js
+++ my-project/config/middlewares.js
@@ -10,3 +10,13 @@ module.exports = [
   "strapi::favicon",
   "strapi::public",
 ];
+
+module.exports = ({ env }) => ({
+  settings: {
+    cors: {
+      enabled: true,
+      origin: env('CORS_ORIGIN', '*'), // You can set specific origins here, or use '*' for any origin
+      headers: ['Content-Type', 'Authorization', 'Accept'],
+    },
+  },
+});
