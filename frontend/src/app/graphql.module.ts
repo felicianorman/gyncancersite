9b77b7c2d6bc2b3f3ee71bce33df4bc7e900da75
Felicia <feelicianorman@hotmail.com>
2024-01-13 18:01:02 +0100

    vercel

--- frontend/src/app/graphql.module.ts
+++ frontend/src/app/graphql.module.ts
@@ -1,9 +1,9 @@
-import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular/http';
 import { NgModule } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
 
-const uri = 'http://localhost:1337/graphql'; // <-- add the URL of the GraphQL server here
+const uri = '/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({ uri }),
--- my-project/config/middlewares.js
+++ my-project/config/middlewares.js
@@ -1,39 +1,12 @@
-
-export default [
-  // The array is pre-populated with internal, built-in middlewares, prefixed by `strapi::`
-  'strapi::logger',
-  'strapi::cors',
-  'strapi::body',
-  'strapi::errors',
-  // ...
-  'my-custom-node-module', // custom middleware that does not require any configuration
-  {
-    // custom name to find a package or a path
-    name: 'my-custom-node-module',
-    config: {
-      foo: 'bar',
-    },
-  },
-  {
-    // custom resolve to find a package or a path
-    resolve: '../some-dir/custom-middleware',
-    config: {
-      foo: 'bar',
-    },
-  },
+module.exports = [
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
-
-module.exports = ({ env }) => ({
-  settings: {
-    cors: {
-      enabled: true,
-      origin: env('CORS_ORIGIN', 'https://gyncancersite.vercel.app'), // Set your frontend origin
-      headers: ['Content-Type', 'Authorization', 'Accept'],
-    },
-  },
-  load: {
-    before: ['strapi::cors', 'strapi::logger', 'strapi::body', 'strapi::errors'],
-    order: ['Define the order in which middleware should be applied'],
-  },
-});
-
